a38162ee4f739fdb9703793220fe9d06eef47a63
Ermias Haimanot <ermiashtsegaye@gmail.com>
2025-08-27 14:54:26 +0300

    create the functions i need based on SOLID principles

---
+++ script.js
@@ -0,0 +1,35 @@
+const cells = document.querySelectorAll('.cell');
+const restartBtn = document.querySelector('.restartButton');
+
+const winningPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+let cellContents = ['', '', '', '', '', '', '', '', ''];
+
+function startGame() {
+
+}
+
+function updateCell() {
+
+}
+
+function cellClicked() {
+
+}
+
+function checkWinner() {
+
+}
+
+function restartGame() {
+
+}
